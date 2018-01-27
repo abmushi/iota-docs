dfc2357d9dcd0b00d780f3cef1779d07523d8e00
ABmushi <9206551+abmushi@users.noreply.github.com>
2018-01-26 22:02:55 -0700

    Create address.js

---
+++ qiita/multisig/address.js
@@ -0,0 +1,91 @@
+//  This file should be placed at iota.lib.js/example/
+var IOTA = require('../lib/iota');
+var Signing = require('../lib/crypto/signing/signing');
+var Converter = require('../lib/crypto/converter/converter');
+var crypto = require("crypto");
+var fs = require('fs');
+var rl = require('readline-sync');
+
+//	オフラインでも動く。
+var iota = new IOTA();
+
+//	seedを入力する。
+const seeds = function(){
+	obj = {};
+
+	var numOfSeed = rl.questionInt('How many seeds? ( 1 以上)');
+
+	if(numOfSeed < 1){
+		return null;
+	}
+
+	obj.seeds = [];
+
+	var seed,security;
+	for(var i = 0;i<numOfSeed;i++){
+		seed = rl.questionNewPassword('Seed: ',{charlist:'$<A-Z>9',mask:'#',min:81,max:81});
+        security = rl.questionInt('Security(1~3): ');
+
+        if(security < 1){
+        	return null;
+        }
+
+        obj.seeds.push({"seed":seed,"security":security});
+
+        console.log('- Make sure you have HAND-WRITTEN your SEED!!');
+	}
+
+	obj.index = rl.questionInt('current index: ');
+
+	if(obj.index < 0){
+		return null;
+	}
+
+	return obj;
+}
+
+//	アドレスを作成する。
+const next = function(login,currentIndex){
+	var digestList = [];
+	login.seeds.forEach(function(each){
+		digestList.push(iota.multisig.getDigest(each.seed, currentIndex, each.security));
+	});
+
+	var Address = iota.multisig.address;
+
+	var d = '';
+	digestList.forEach(function(each){
+		d += each;
+	});
+
+	var digests = Converter.trits(d);
+	var address = Converter.trytes(Signing.address(digests));
+
+	//	生成されたアドレスは有効か確認。
+	var isValid = iota.multisig.validateAddress(address, digestList);
+	if(isValid){
+		console.log('- - - - - - - - - - - - - - - - - - - - ');
+		console.log(' - index: ',currentIndex);
+		console.log(' - address: ',address);
+		console.log('- - - - - - - - - - - - - - - - - - - - ');
+	}else{
+		console.log('Error: Invalid address.');
+	}
+}
+
+var login = seeds();
+
+if(login != null){
+	var index = login.index;
+	while(true){
+		next(login,index);
+		if(!rl.keyInYN('Want next index?')){
+			break; 
+		}
+		index++;
+	}
+}else{
+	console.log('Error: Invalid infomation input.');
+}
+
+
